cabeaa3e0785f0e0f7ca4b6345ba5804e3eb506c
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-18 20:16:30 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
   # your code here
   array1 = []
   array.each do |numbers|
-    numbers=numbers**2 
+    numbers=numbers**2
     array1<<numbers
- 
-end   
+
+end
 array1
 end
