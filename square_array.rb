96b7f55ddf63f5d9930f105836582cf9a6b67963
Keren-Happuc Fleurimond <keren@noukod.com>
2018-09-18 20:13:45 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   # your code here
-  array1 = []
-  array.each { |num| array1 << num ** 2 }
-  array1
+  new_array = []
+  array.each { |num| new_array << num ** 2 }
+  new_array
 
 end
 end
